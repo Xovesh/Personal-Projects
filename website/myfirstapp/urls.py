f72abd9d4e2d74cb1ba80a911b1d1dea3d7216fb
Xovesh <39794796+Xovesh@users.noreply.github.com>
2018-08-04 10:10:11 +0200

    04/08/2018

--- website/myfirstapp/urls.py
+++ website/myfirstapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('<pname>', views.details, name="details")
+    path('', views.pokedex, name="pokedex"),
+    path('pokemon/<pname>', views.details, name="details"),
 ]
--- website/myfirstapp/views.py
+++ website/myfirstapp/views.py
@@ -3,15 +3,44 @@ from .models import Pokemon
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
+def pokedex(request):
     all_pokemons = Pokemon.objects.all()
     context = {"all_pokemons" : all_pokemons}
     return render(request, "myfirstapp/pokedex.html", context)
 
 def details(request, pname):
-    try:
-        pokemon = Pokemon.objects.all().filter(name=pname)
-        context = {"pokemon": pokemon}
-        return render(request, "myfirstapp/details.html", context)
-    except:
-        return HttpResponse("<h1>404 not found</h1>")
+    pokemon = Pokemon.objects.all().filter(name=pname)
+
+    #For evolutions
+    evolutions = [pokemon[0]]
+    i = 1
+    if pokemon[0].firstevolution == 1:
+        auxpok = Pokemon.objects.all().filter(pk=pokemon[0].pk + i)
+        while auxpok[0].firstevolution != 1:
+            evolutions.append(auxpok[0])
+            i += 1
+            auxpok = Pokemon.objects.all().filter(pk=pokemon[0].pk + i)
+    else:
+        auxpok = Pokemon.objects.all().filter(pk=pokemon[0].pk - i)
+        while auxpok[0].firstevolution != 1:
+            evolutions.insert(0, auxpok[0])
+            i += 1
+            auxpok = Pokemon.objects.all().filter(pk=pokemon[0].pk - i)
+        evolutions.insert(0, auxpok[0])
+        i = 1
+        auxpok = Pokemon.objects.all().filter(pk=pokemon[0].pk + i)
+        while auxpok[0].firstevolution != 1:
+            evolutions.append(auxpok[0])
+            i += 1
+            auxpok = Pokemon.objects.all().filter(pk=pokemon[0].pk + i)
+
+    context = {"pokemon": pokemon, "evolutions": evolutions}
+    return render(request, "myfirstapp/details.html", context)
+
+
+
+
+
+
+
+
