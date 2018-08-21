e24cae82fe98f2ff474e6dfe444feaa7b93eafbc
Xovesh <39794796+Xovesh@users.noreply.github.com>
2018-08-21 13:05:01 +0200

    8/21/2018

--- website/website/views.py
+++ website/website/views.py
@@ -5,10 +5,4 @@ from django.http import HttpResponse
 
 def mainpage(request):
 
-    #return render(request, "index.html")
-    return HttpResponse("""<h1>This is the main page!</h1>""")
-    #                     <br>
-    #                     <a href="http://127.0.0.1:8000/myfirstapp/">pokedex</a>
-    #                     <br>
-    #                     <a href="http://127.0.0.1:8000/admin/">admin</a>
-    # """)
+    return render(request, "index.html")
